4a2c20acca10e8fe532adb288c663ddf7ebab180
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-06-05 01:01:50 -0300

    lançamento de exceções para datas inválidas

--- app.js
+++ app.js
@@ -26,13 +26,30 @@ function cadastrarDespesa() {
     let ano = document.getElementById('ano'); 
     let mes = document.getElementById('mes'); 
     let dia = document.getElementById('dia'); 
-    let data = new Date(ano.value, mes.value-1, dia.value); 
+    let data; 
+    try {
+        if (!isNumeric(ano.value, mes.value, dia.value)) {
+            throw 'Data não informada corretamente. \nCampos de data solicitados não foram preenchidos.'; 
+        }
+        data = new Date(ano.value, mes.value-1, dia.value); 
+        if (data.getDate() != dia.value || data.getMonth()+1 != mes.value || data.getFullYear() != ano.value) {
+            throw 'Data informada não corresponde a uma data válida.'
+        }
+        
+    } catch (e) {
+        data = new Date(); 
+        alert(`${e} \n\nA despesa foi cadastrada na data de hoje (${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()})`); 
+    }
     let tipo = document.getElementById('tipo'); 
     let descricao = document.getElementById('descricao'); 
     let valor = document.getElementById('valor'); 
     
-    let despesa = new Despesa(data, tipo, descricao, valor); 
-    console.log(despesa); 
-
+    try {
+        var despesa = new Despesa(data, tipo.value, descricao.value, valor.value); 
+    } catch (e) {
+        console.log(`Não foi possível criar o objeto. \n${e}`); 
+        return null; 
+    }
+    // console.log(despesa); 
     return despesa; 
 }
