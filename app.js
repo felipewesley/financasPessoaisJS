b970301b3593d78acc690ce0034d1768c3d03966
Felipe Wesley <fe.wesleybasso@gmail.com>
2020-06-08 01:47:46 -0300

    validação das entradas do form

--- app.js
+++ app.js
@@ -1,11 +1,85 @@
 class Despesa {
-    constructor(data, tipo, descricao, valor) {
+
+    constructor(dia, mes, ano, tipo, descricao, valor) {
+        
+        this.status = true; 
+
+        if (!this.validarInformacoes(tipo, descricao, valor)) {
+            this.setStatus(false)
+            return false; 
+        }
+
+        let data = this.validarData(dia, mes, ano); 
+        
+        if (!data){
+            this.setStatus(false)
+            return false; 
+        }
+
         this.ano = data.getFullYear()
         this.mes = data.getMonth()
         this.dia = data.getDate()
         this.tipo = tipo
         this.descricao = descricao
         this.valor = valor
+    }
+
+    dataIsNumeric (...dados) {
+        if (dados.length > 0) {
+            for(const e in dados) {
+                // console.log(dados[e]); 
+                if (isNaN(parseInt(dados[e]))){
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+    
+    validarData(dia, mes, ano) {
+        
+        let data = new Date(); 
+
+        if (!this.dataIsNumeric(dia, mes, ano)) {
+            let validacao = `A data não foi informada corretamente.\n
+                        Deseja cadastrar esta despesa na data de hoje? 
+                        (${new Date().toDateString()})`; 
+            if (confirm(validacao)) {
+                return data; 
+            }
+        } else{
+            data.setDate(dia); 
+            data.setMonth(mes-1); 
+            data.setFullYear(ano); 
+            
+            return data; 
+        }
+        
+        return false; 
+    }
+
+    isEmpty(str) {
+        if (str == ''.trim()) {
+            return true; 
+        }
+        return false; 
+    }
+
+    validarInformacoes(...dados) {
+        for(const i in dados) {
+            if(this.isEmpty(dados[i])) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+
+    getStatus() {
+        return this.status; 
+    }
+
+    setStatus(status) {
+        this.status = status; 
     }
 }
 
@@ -28,49 +102,32 @@ class Database {
     }
 }
 
-function isNumeric (...dados) {    
-    if (dados.length > 0) {
-        for(const e in dados) {
-            console.log(dados[e]); 
-            if (isNaN(parseInt(dados[e]))){
-                return false; 
-            }
-        }
-    }
-    return true;
-}
-
 const bd = new Database(); 
 
 function cadastrarDespesa() {
     
-    let ano = document.getElementById('ano'); 
-    let mes = document.getElementById('mes'); 
     let dia = document.getElementById('dia'); 
-    let data; 
-    try {
-        if (!isNumeric(ano.value, mes.value, dia.value)) {
-            throw 'Data não informada corretamente. \nCampos de data solicitados não foram preenchidos.'; 
-        }
-        data = new Date(ano.value, mes.value-1, dia.value); 
-        if (data.getDate() != dia.value || data.getMonth()+1 != mes.value || data.getFullYear() != ano.value) {
-            throw 'Data informada não corresponde a uma data válida.'
-        }
-        
-    } catch (e) {
-        data = new Date(); 
-        alert(`${e} \n\nA despesa foi cadastrada na data de hoje (${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()})`); 
-    }
+    let mes = document.getElementById('mes'); 
+    let ano = document.getElementById('ano'); 
     let tipo = document.getElementById('tipo'); 
     let descricao = document.getElementById('descricao'); 
     let valor = document.getElementById('valor'); 
     
-    try {
-        var despesa = new Despesa(data, tipo.value, descricao.value, valor.value); 
-    } catch (e) {
-        console.log(`Não foi possível criar o objeto. \n${e}`); 
-        return null; 
+    let despesa = new Despesa(dia.value, mes.value, ano.value, tipo.value, descricao.value, valor.value); 
+    
+    if(despesa.getStatus()){
+        console.log(despesa); 
+        alert('Despesa cadastrada com sucesso'); 
+        dia.value = ''; 
+        mes.value = ''; 
+        ano.value = ''; 
+        tipo.value = ''; 
+        descricao.value = ''; 
+        valor.value = ''; 
+        // return bd.gravar(despesa); 
+    } else{
+        console.log('validacao: tem coisa vazia'); 
+        // return alert('Não foi possível criar a despesa.'); 
     }
     
-    return bd.gravar(despesa); 
 }
